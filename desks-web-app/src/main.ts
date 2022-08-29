06733e015f72206737709111d4c1fc21d133a71f
Becker, Christoph <christoph@chb-do.de>
2022-08-29 10:30:04 +0200

    feat: package used fonts

--- desks-web-app/src/main.ts
+++ desks-web-app/src/main.ts
@@ -1,11 +1,9 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import {loadFonts} from './plugins/webfontloader'
 import {key, store} from "./stores/store";
 import {stringFormat} from "@/plugins/string-formatter";
 
-loadFonts()
 
 createApp(App)
     .use(vuetify)
