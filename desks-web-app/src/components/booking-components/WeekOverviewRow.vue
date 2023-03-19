46152a149e6d0e35b60e9c295f35a53b9d3f389c
Christoph Becker <post@christophb.de>
2023-03-19 11:09:54 +0100

    feat: add booking week window animation

--- desks-web-app/src/components/booking-components/WeekOverviewRow.vue
+++ desks-web-app/src/components/booking-components/WeekOverviewRow.vue
@@ -4,11 +4,12 @@
   <v-row class="mt-4">
     <v-col>
       <h1 class="text-h4">{{ isPreviousWeekDisabled() ? "This Week" : "Week Overview" }}</h1>
-      <h3 class="text-subtitle-1">{{$format.date(calculateNthDayOfWeek(1))}} - {{$format.date(calculateNthDayOfWeek(5))}}</h3>
+      <h3 class="text-subtitle-1">{{$format.date(calculateNthDayOfWeek(1))}} - {{$format.date(calculateNthDayOfWeek(5))}} | {{bookingDefaults?.room?.name}}</h3>
     </v-col>
 
     <v-col align-self="center" class="d-flex justify-end">
-      <v-btn icon variant="text" @click="()=>previousWeek()"
+      <v-btn icon variant="text"
+             @click="()=>previousWeek()"
              :disabled="isPreviousWeekDisabled()">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
@@ -18,72 +19,58 @@
     </v-col>
   </v-row>
   <v-row>
-    <template v-for="n in 5" :key="n">
-      <v-col>
-        <availability-card
-            v-if="this.bookingDefaults"
-            :startOfDay="calculateNthDayOfWeek(n)"
-            :room="this.bookingDefaults.room"
-            @add-booking="bookForDay(calculateNthDayOfWeek(n))"
-        ></availability-card>
-      </v-col>
-    </template>
+    <v-col>
+      <v-window v-model="window">
+        <v-window-item v-for="(week,n) in weeks" :value="n" :key="n">
+          <week-overview-window :start-of-week="week"  @add-booking="(booking) => $emit('book', booking)"></week-overview-window>
+        </v-window-item>
+      </v-window>
+    </v-col>
+
   </v-row>
 
 </template>
 <script lang="ts">
 import {defineComponent} from "vue";
-import {mapActions, mapGetters, mapState} from "vuex";
 import moment from "moment/moment";
-import BookingUtils from "@/utils/booking-utils";
-import Booking from "@/models/Booking";
 import {Moment} from "moment";
-import AvailabilityCard from "@/components/booking-components/AvailabilityCard.vue"
+import WeekOverviewWindow from "@/components/booking-components/WeekOverviewWindow.vue";
+import {mapState} from "vuex";
 
 interface DeskAvailabilityState {
-  overlappingBookings: number | null
-  bookingsOfColleagues: Booking[]
   startOfWeek: Moment
+  weeks: Moment[]
+  window: number
 }
 
 export default defineComponent({
   name: 'desk-availability',
-  components: {AvailabilityCard},
+  components: {WeekOverviewWindow},
   data: (): DeskAvailabilityState => ({
-    overlappingBookings: null,
-    bookingsOfColleagues: [],
+    window: 0,
+    weeks: [moment().startOf("week"), moment().startOf("week").add(1, "week")],
     startOfWeek: moment().startOf("week")
   }),
-  computed: {
-    ...mapState("defaults", ["bookingDefaults", "bookingDefaultsLoading"]),
-    ...mapGetters("bookings", ["getBookingsByRoomAndDay"]),
-    ...mapState("bookings", ["bookings"]),
-    ...mapState('user', ['user']),
-  },
-  mounted() {
 
-    this.fetchBookingDefaults()
-    this.fetchBookings()
-  },
-  watch: {
-    async bookingDefaults() {
-      await this.fetchBookings()
-      await this.loadAndCheck()
-    },
+  computed:{
+    ...mapState("defaults",["bookingDefaults"])
   },
   methods: {
-
-    ...mapActions("defaults", ["fetchBookingDefaults"]),
-    ...mapActions("bookings", ["fetchBookingsByRoomAndDate"]),
-
     nextWeek() {
-      this.startOfWeek = moment(this.startOfWeek).add(1, "week")
+      if(this.window == this.weeks.length - 2){
+        this.weeks = [...this.weeks,moment(this.weeks[this.weeks.length-1]).add(1, "week")]
+      }
+      this.window++
+
+      this.startOfWeek = this.weeks[this.window]
     },
     previousWeek() {
-      this.startOfWeek = moment(this.startOfWeek).subtract(1, "week")
+      this.window--
+      this.startOfWeek = this.weeks[this.window]
+
     },
     isPreviousWeekDisabled(): boolean {
-      return this.startOfWeek.isSameOrBefore(moment().startOf("week"))
+      return this.window <= 0
     },
     isDisabled: (startOfDay: Moment): boolean => startOfDay.isBefore(moment().startOf("day")),
 
@@ -91,58 +78,6 @@ export default defineComponent({
       return moment(this.startOfWeek).add(n, "day")
     },
 
-    bookForDay: function (startOfDay: Moment) {
-      const startDefault = moment(this.bookingDefaults.start);
-      let start = startOfDay.startOf("day").add(startDefault.hour(), "hour").add(startDefault.minutes(), "minute")
-      start = start.isBefore(moment.now()) ? BookingUtils.roundToNextFiveMinutes() : start
-
-      const endDefault = moment(this.bookingDefaults.end);
-      let end = moment().startOf("day").add(endDefault.hour(), "hour").add(endDefault.minutes(), "minute")
-      end = end.isSameOrAfter(moment.now()) && end.isSameOrAfter(start) ? end : moment(start).add(1, "hour")
-      const booking: Partial<Booking> = {
-        room: this.bookingDefaults.room,
-        start: start.toDate(),
-        end: end.toDate()
-      }
-      this.$emit("book", booking)
-    },
-
-    async fetchBookings() {
-      if (this.bookingDefaults && this.bookingDefaults.room) {
-        await this.fetchBookingsByRoomAndDate({roomId: this.bookingDefaults.room.id, date: moment.now()})
-      }
-    },
-    async loadAndCheck() {
-
-      if (this.bookingDefaults?.room?.id) {
-
-        this.checkOverlaps(this.getBookingsByRoomAndDay(this.bookingDefaults.room.id, moment.now()))
-      }
-    },
-
-    checkOverlaps: function (bookings: Array<Booking>) {
-
-      const start = moment().startOf("day")
-      const end = moment().endOf("day")
-      this.bookingsOfColleagues = bookings.filter(value => value.user.id !== this.user.id)
-      const overlaps = BookingUtils.findOverlaps(bookings, start, end)
-      this.overlappingBookings = new Set(overlaps.map(booking => booking.user.id)).size
-    },
-
-    calculateType(startOfDay: moment.Moment, number: number): string {
-      if (this.isDisabled(startOfDay)) {
-        return ""
-      }
-      if (number >= this.bookingDefaults.room.capacity) {
-        return "red"
-      }
-      if (number > this.bookingDefaults.room.capacity * 0.8) {
-        return "orange"
-      }
-
-      return "success"
-
-    }
   }
 });
 </script>
