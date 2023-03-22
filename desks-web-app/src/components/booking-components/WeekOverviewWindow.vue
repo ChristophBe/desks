cf0697c6ac7d6748b575251c0cd3b00a44f1b8a4
Rickmann, Bastian <bastian.rickmann@umlaut.com>
2023-03-22 15:55:26 +0100

    feat: change to day notes for today and mondays

--- desks-web-app/src/components/booking-components/WeekOverviewWindow.vue
+++ desks-web-app/src/components/booking-components/WeekOverviewWindow.vue
@@ -3,7 +3,8 @@
   <v-row>
     <template v-for="n in [0,1,2,3,4,5,6]" :key="n">
       <v-col  :class="{days: true, monday: calculateNthNextDay(n).isoWeekday() === 1}" v-if="this.bookingDefaults && calculateNthNextDay(n).isoWeekday() <= 5">
-        <span id="nextWeek">Next Week</span>
+        <span id="dayNote" v-if="calculateNthNextDay(n).startOf('day').isSame(today.startOf('day'), 'day')">Today</span>
+        <span id="dayNote" v-else>KW {{calculateNthNextDay(n).isoWeek()}}</span>
         <availability-card
             v-if="this.bookingDefaults"
             :startOfDay="calculateNthNextDay(n)"
@@ -34,7 +35,7 @@
 }
 
 
-#nextWeek {
+#dayNote {
   display: none;
   margin-bottom: 1em;
   color: #585858;
@@ -42,12 +43,8 @@
   font-size: 1em;
 }
 
-.monday #nextWeek, .days:first-child #nextWeek {
+.monday #dayNote, .days:first-child #dayNote {
   display: block;
-}
-
-.days:first-child #nextWeek {
-    visibility: hidden;
 }
 
 </style>
@@ -75,11 +72,9 @@ export default defineComponent({
     ...mapGetters("bookings", ["getBookingsByRoomAndDay"]),
     ...mapState("bookings", ["bookings"]),
     ...mapState('user', ['user']),
+    today: () => moment(),
   },
   async mounted() {
-
-    console.log(this.startOfWeek);
-
     await this.fetchBookingDefaults()
     await this.fetchBookings()
   },
@@ -96,7 +91,6 @@ export default defineComponent({
     isDisabled: (startOfDay: Moment): boolean => startOfDay.isBefore(moment().startOf("day")),
 
     calculateNthNextDay(n: number): Moment {
-      console.log(moment(this.startOfWeek).add(n, "day"))
       return moment(this.startOfWeek).add(n, "day")
     },
 
